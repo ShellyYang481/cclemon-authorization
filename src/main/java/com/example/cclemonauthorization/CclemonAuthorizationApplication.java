eda23530f0bb01e05f9a7f7972ad4a2d183017fe
Shelly <shelly.yang@alp.global>
2024-03-24 21:37:29 +0800

    Initial commit

---
+++ src/main/java/com/example/cclemonauthorization/CclemonAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.cclemonauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclemonAuthorizationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CclemonAuthorizationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/cclemonauthorization/CclemonAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cclemonauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CclemonAuthorizationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
